a72654cc2268b21044221011cf861b046920fdab
lugy <naitianzong@outlook.com>
2014-12-27 15:02:58 +0800

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,12 +5,11 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
-  def index
-    @users = User.all
-  end
+
   def create
     @user = User.new(user_params)
     if @user.save
+      sign_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
